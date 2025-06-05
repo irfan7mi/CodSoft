a42d9bcdb2cdd626a2e37864dd5894b7cc3cca72
Mohamed Irfan <irfan@Mohameds-MacBook-Air.local>
2025-06-05 23:45:31 +0530

    Initial commit

--- Level2/JobBoard/BackEnd/index.js
+++ Level2/JobBoard/BackEnd/index.js
@@ -5,6 +5,7 @@ const authRoutes = require('./routes/authRoute');
 const jobRoutes = require('./routes/jobRoute');
 const applnRoutes = require('./routes/applnRoute');
 const savedJobRoutes = require('./routes/savedJobRoute');
+const UserModel = require('./models/users');
 const url = process.env.MONGO_URI || 'mongodb+srv://mi2268242:q0zQ2HuspFPfohf0@doorfood.gxuxa.mongodb.net/?retryWrites=true&w=majority&appName=bazario';
 const app = express();
 app.get('/', (req, res) => {
@@ -21,6 +22,64 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+
+const createToken = (id) => {
+  return jwt.sign({id}, JWT_SECRET)
+}
+
+app.post("api/auth/login", async (req, res) => {
+  const{email, password} = req.body
+  try{
+    let user = await AdminModel.findOne({email})
+    if (!user) {
+      return res.json({success: false, message: "Admin doesn't exist!"})
+    }
+    const isMatch = bcrypt.compare(email, password)
+    if (!isMatch) {
+      return res.json({success: false, message: "Invalid credentials"})
+    }
+    const token = createToken(user._id)
+    return res.send({success:true, message: "Login successfully", token})
+  }
+  catch (e) {
+    console.log(e)
+    res.send({success: false, message: "Error"})
+  }
+})
+
+app.post("api/auth/signin",async (req, res) => {
+  const {name, mobile, email, password} = req.body
+  try{
+    const exist = await UserModel.findOne({email})
+    if (exist) {
+      return res.json({success:false, message:"User already exist!"})
+    }
+    if (!validator.isEmail(email)) {
+      return res.json({success:false, message:"Please enter valid email address!"})
+    }
+    if (password.length<8) {
+      return res.json({success:false, message:"Please enter strong password!"})
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    
+    const newUser = new UserModel({
+      name: name,
+      mobile: mobile,
+      email: email,
+      password: hashedPassword,
+      role: 'candidate' 
+    })
+    let user = await newUser.save()
+    const token = createToken(user._id)
+    return res.send({user: { name: candidate.name, email: candidate.email, role: candidate.role },
+      token})
+  }
+  catch (e) {
+    console.log(e)
+    res.send({success:false, message: "Error"})
+  }
+})
 
 mongoose.connect(url)
   .then(() => console.log("Connected to MongoDB"))
--- Level2/JobBoard/BackEnd/routes/authRoute.js
+++ Level2/JobBoard/BackEnd/routes/authRoute.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController');
 
-router.post('/login', authController.login);
-router.post('/signup', authController.signup);
 router.post('/profile', authController.updateProfile);
 
 module.exports = router;
