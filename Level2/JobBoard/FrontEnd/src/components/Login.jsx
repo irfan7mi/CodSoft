1e3e0c7725e6a72efe2120a66cbc0021346a53be
Mohamed Irfan <irfan@Mohameds-MacBook-Air.local>
2025-06-06 11:16:47 +0530

    Initial commit

--- Level2/JobBoard/FrontEnd/src/components/Login.jsx
+++ Level2/JobBoard/FrontEnd/src/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = ({ boolLogin, setBoolLogin, setUser }) => {
     e.preventDefault();
     setError('');
     const endpoint = isSignUp ? '/api/auth/signup' : '/api/auth/login';
-    const response = await axios.post(`https://job-board-server-nine.vercel.app${endpoint}`, formData);
+    const response = await axios.post(`https://codsoft-fctc.onrender.com${endpoint}`, formData);
     if(response.data.success){
       const userData = {
         name: response.data.user.name || 'John Doe',
