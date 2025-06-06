956aa7d3e497b48875cbc28251416f994f708e7f
Mohamed Irfan <irfan@Mohameds-MacBook-Air.local>
2025-06-06 08:31:30 +0530

    Initial commit

--- Level2/JobBoard/BackEnd/index.js
+++ Level2/JobBoard/BackEnd/index.js
@@ -94,7 +94,7 @@ app.use('/api/jobs', jobRoutes);
 app.use('/api/application', applnRoutes);
 app.use('/api/saved-jobs', savedJobRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
